f313e121ac0fad85821e5b7de1d8549fc340e5fe
mshereme <mshereme@debian.com>
2024-07-18 18:03:09 +0200

    finished ?

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -5,11 +5,9 @@ service mariadb start
 sleep 3
 # systemctl status mariadb
 
-# echo " TEST MARIADB "
-
 mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 
-mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '$(cat /run/secrets/db_password)';"
 
 mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
 
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,41 +1,13 @@
 #!bin/bash
 
-# cd /var/www/html
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
-chmod +x wp-cli.phar
-
-mv	wp-cli.phar /usr/bin/wp
-
 wp core download --allow-root
 
-sleep 3
+sleep 10
 
-wp config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost="mariadb" --allow-root
+wp core config --dbname=${SQL_DATABASE} --dbuser=${SQL_USER} --dbpass=$(cat /run/secrets/db_password) --dbhost="mariadb" --allow-root
 
-wp core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+wp core install --url=mshereme.42.fr --title=${SQL_DATABASE} --admin_user=${SQL_ROOT} --admin_password=$(cat /run/secrets/db_root_password) --admin_email=god@gmail.com --allow-root
+
+wp user create mshereme mshereme@lol.com --role=author --user_pass=$(cat /run/secrets/db_password) --allow-root
 
 exec /usr/sbin/php-fpm7.4 -F;
-
-
-# wp core download --allow-root
-
-# sleep 3
-# wp core config    --dbname=MYSQL_DATABASE\
-# 				--dbhost="mariadb"\
-# 				--dbuser=MYSQL_USER\
-# 				--dbpass=MYSQL_PASS\
-# 				--allow-root
-
-# wp core install --url=acronert.42.fr\
-# 				--title=WP_TITLE\
-# 				--admin_user=WP_ADMIN\
-# 				--admin_password=WP_ADMIN_PASS\
-# 				--admin_email=WP_ADMIN_EMAIL\
-# 				--allow-root
-
-# wp user create WP_USER WP_USER_EMAIL --role=author\
-# 				--user_pass=WP_USER_PASS\
-# 				--allow-root
-
-# exec /usr/sbin/php-fpm7.4 -F;
